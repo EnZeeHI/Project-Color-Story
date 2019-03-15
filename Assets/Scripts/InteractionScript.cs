b46565096f8ab968a3b14c3c79e8149ac5d2c1bf
EnZeeHI <34064073+EnZeeHI@users.noreply.github.com>
2019-03-15 10:45:13 +0100

    Cleaned up the file structure, added detection for different interactables
    - moved all the scripts to the scripts folder
    -moved all the scenes into the scenes folder
    -added detection for different types of interactables(interactable objects, Items and people)

--- Assets/Scripts/InteractionScript.cs
+++ Assets/Scripts/InteractionScript.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class InteractionScript : MonoBehaviour
 {
 
-
-    public GameObject InteractionPrompt;
-    private GameObject InteractionPromptObject;
+    // Defining prefabs and Object this script is going to create, adding an offset for prompts adding boolean values for future use
+    public GameObject InteractionObjectPrompt;
+    private GameObject InteractionObjectPromptObject;
     public Vector3 offset = new Vector3(0,1,0);
     private bool isCreated;
-    public bool InteractionPromptActive;
-    public
+    public bool InteractionObjectPromptActive;
+    public GameObject QuestItemPromptPrefab;
+    private GameObject QuestItemPromptObject;
+    public GameObject QuestPersonPromptPrefab;
+    private GameObject QuestPersonPromptObject;
+    public bool QuestItemPromptObjectActive;
+    public bool QuestPersonPromptObjectActive;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // temporarilly assigning the objects to get rid of the "unassigned reference" error
+        InteractionObjectPromptObject = InteractionObjectPrompt;
+        QuestItemPromptObject = QuestItemPromptPrefab;
+        QuestPersonPromptObject = QuestPersonPromptPrefab;
     }
 
     // Update is called once per frame
@@ -23,58 +31,100 @@ public class InteractionScript : MonoBehaviour
     {
          RaycastHit hit;
         // Does the ray intersect any objects excluding the player layer
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2))
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 3))
         {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-           
-
-            
+        // checking if the raycast is detecting the Quest Object, Instantiating a prompt once, and seting it active/unactive based on if the player aims at the object or not 
             if (hit.collider.tag == "QuestObject"  )
             {
                 
                 if ( hit.transform.childCount <1)
                 {   
-                   
-                    
-                    InteractionPromptObject = Instantiate(InteractionPrompt, hit.collider.transform.position+ offset, hit.collider.transform.rotation );
-                    InteractionPromptObject.transform.parent = hit.transform;
-                    isCreated = true;
-                    
-                    
+                    InteractionObjectPromptObject = Instantiate(InteractionObjectPrompt, hit.collider.transform.position+ offset, hit.collider.transform.rotation );
+                    InteractionObjectPromptObject.transform.parent = hit.transform;
+                    isCreated = true;                    
                 }
                
                if(hit.transform.childCount ==1)
                {
-                  InteractionPromptObject =  hit.transform.Find("InteractionPrompt(Clone)").gameObject;
+                  InteractionObjectPromptObject =  hit.transform.Find("InteractionObjectPrompt(Clone)").gameObject;
                }
-                InteractionPromptObject.SetActive(true);
-                InteractionPromptActive = true;
-                Debug.Log("active");
-
+                InteractionObjectPromptObject.SetActive(true);
+                InteractionObjectPromptActive = true;
+               // Debug.Log("active")
             }
             else
             {
-               InteractionPromptObject.SetActive(false);
-               InteractionPromptActive = false;
-               Debug.Log("disabled but active"); 
+            // making sure that the prompt disapears if the raycast hits something else than the required object
+               InteractionObjectPromptObject.SetActive(false);
+               InteractionObjectPromptActive = false;
+              // Debug.Log("Not InteractionObject"); 
             }
-
-            if (InteractionPromptActive)
+            // this the interaction with the object
+            if (InteractionObjectPromptActive)
+            {
+                if (Input.GetKeyDown("e"))
+                Destroy(hit.transform.gameObject);
+            }
+            // checking if the raycast detects a quest item, istantiating the prompt and toggling the active state of the prompt based on if the player is aiming at it or not
+        if (hit.collider.tag=="QuestItem")
         {
-            if (Input.GetKeyDown("e"))
-            Destroy(hit.transform.gameObject);
+            if ( hit.transform.childCount <1)
+            {   
+                QuestItemPromptObject = Instantiate(QuestItemPromptPrefab, hit.collider.transform.position+ offset, hit.collider.transform.rotation );
+                QuestItemPromptObject.transform.parent = hit.transform;
+                isCreated = true;              
+            }
+            if(hit.transform.childCount ==1)
+            {
+                QuestItemPromptObject =  hit.transform.Find("QuestItemPromptPrefab(Clone)").gameObject;
+            }
+            QuestItemPromptObject.SetActive(true);
+            QuestItemPromptObjectActive = true;
+           // Debug.Log("active"); 
+        }
+        // doing the same check as before
+        else
+        {
+            QuestItemPromptObject.SetActive(false);
+            QuestItemPromptObjectActive = false;
+           // Debug.Log("Not Quest Item"); 
+        }
+        // same as before but for a quest person 
+        if (hit.collider.tag=="QuestPerson")
+        {
+            if ( hit.transform.childCount <1)
+            {   
+                QuestPersonPromptObject = Instantiate(QuestPersonPromptPrefab, hit.collider.transform.position+ offset, hit.collider.transform.rotation );
+                QuestPersonPromptObject.transform.parent = hit.transform;
+                isCreated = true;              
+            }
+            if(hit.transform.childCount ==1)
+            {
+                QuestPersonPromptObject =  hit.transform.Find("QuestPersonPromptPrefab(Clone)").gameObject;
+            }
+            QuestPersonPromptObject.SetActive(true);
+            QuestPersonPromptObjectActive = true;
+            //Debug.Log("active")   
+        }
+        else
+        {
+            QuestPersonPromptObject.SetActive(false);
+            QuestPersonPromptObjectActive = false;
+           // Debug.Log("Not Quest Person"); 
         }
         }
+        // disabling every prompt if the raycast doesnt detect anything
         else
         {   
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            InteractionPromptObject.SetActive(false);
-            InteractionPromptActive = false;
-            Debug.Log("disabled");
-        }
-
-        
-        
+            InteractionObjectPromptObject.SetActive(false);
+            InteractionObjectPromptActive = false;
+            QuestItemPromptObject.SetActive(false);
+            QuestItemPromptObjectActive = false;
+            QuestPersonPromptObject.SetActive(false);
+            QuestItemPromptObjectActive = false;
+            //Debug.Log("disabled");
+        }       
     }
-    
 }
