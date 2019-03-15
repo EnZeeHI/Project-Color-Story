dfcd468ab6d80f4dd8fdfd9386001078d3eddff7
Toxic <marcelweetikveel@hotmail.nl>
2019-03-15 13:47:34 +0100

    text trigger

---
+++ Assets/DialougeManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialougeManager : MonoBehaviour
+{
+    public Text nameText;
+    public Text dialougeText;
+
+    private Queue<string> sentences;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialouge (Dialouge dialouge)
+    {
+
+        nameText.text = dialouge.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialouge.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialouge();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialougeText.text = sentence;
+    }
+
+    void EndDialouge()
+    {
+        Debug.Log("End of conversation");
+    }
+}
