e500fbec7ff44445bbd71fcb8fca5730b154be75
EnZeeHI <34064073+EnZeeHI@users.noreply.github.com>
2019-03-21 21:20:02 +0100

    Added door opening script

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public bool openTheDoor;
+    Vector3 doorOpenVector;
+    Vector3 doorClosedVector;
+    bool doorOpen = false;
+    bool doorClosed = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (openTheDoor)
+        {
+            doorOpenVector = transform.position + new Vector3 (0,0,2);
+            doorClosedVector =  transform.position + new Vector3 (0,0,-2);
+            if (openTheDoor && doorOpen)
+            {
+                transform.position = doorClosedVector;
+                doorClosed = true;
+                doorOpen = false;
+                openTheDoor =false;
+            }
+            if (openTheDoor && doorClosed)
+            {
+                transform.position = doorOpenVector;
+                doorClosed = false;
+                doorOpen  = true;
+                openTheDoor =false;
+            }
+            
+        }
+    }
+}
