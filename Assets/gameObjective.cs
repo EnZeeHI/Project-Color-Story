231529591b9d353e57e9ed19960218f828da55b5
Toxic <marcelweetikveel@hotmail.nl>
2019-06-10 03:14:43 +0200

    Update on pause & exhibit

---
+++ Assets/gameObjective.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class gameObjective : EventTrigger
+{
+    public Text gameObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override void OnPointerEnter(PointerEventData eventdata)
+    {
+        gameObj.text = "go fuck yourself";
+    }
+
+    public override void OnPointerExit(PointerEventData eventdata)
+    {
+        gameObj.text = "Hint:";
+    }
+}
